85dc3389991c1884fdc279b5c88c46b82f160a6b
hannah.lin@afusion.com <hannah.lin@afusion.com>
2018-07-27 14:18:55 +0800

    fix animation

--- src/pages/Page3/index.vue
+++ src/pages/Page3/index.vue
@@ -23,39 +23,39 @@ export default {
     var timeCircle = new TimelineMax();
 
     //Detail Animation
-    // timeSquire.from('.square', 1.2, {
-    //     top: '150%',
-    //     delay: 1.5
-    // })
-    // .to('.square', 5, {
-    //   rotation: 180,
-    //   delay: 0.3,
-    //   ease: Power3.easeOut
-    // })
+    timeSquire.from('.square', 1.2, {
+        top: '150%',
+        delay: 1.5
+    })
+    .to('.square', 5, {
+      rotation: 180,
+      delay: 0.3,
+      ease: Power3.easeOut
+    })
 
-    // timeTriangle.from('.triangle', 1.2, {
-    //     top: '150%',
-    //     delay: 1.5,
-    //     ease: Power3.easeOut
-    // })
-    // .to('.triangle', 5, {
-    //   rotation: 360,
-    //   delay: 0.3
-    // })
+    timeTriangle.from('.triangle', 1.2, {
+        top: '150%',
+        delay: 1.5,
+        ease: Power3.easeOut
+    })
+    .to('.triangle', 5, {
+      rotation: 360,
+      delay: 0.3
+    })
 
-    // timeCircle.from('.mainShape', 1, {
-    //     x: '100%',
-    //     delay: 0.5
-    // })
-    // .from('.circle', 1.2, {
-    //     top: '150%',
-    //     delay: 1,
-    //     ease: Power3.easeOut
-    // })
-    // .to('.circle', 5, {
-    //   scale: 1.1,
-    //   delay: 0.3
-    // })
+    timeCircle.from('.mainShape', 1, {
+        x: '100%',
+        delay: 0.5
+    })
+    .from('.circle', 1.2, {
+        top: '150%',
+        delay: 1,
+        ease: Power3.easeOut
+    })
+    .to('.circle', 5, {
+      scale: 1.1,
+      delay: 0.3
+    })
 
   },
   methods: {
